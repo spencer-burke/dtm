5bafe1deae99d8130e264114359c6802d5b6c383
Spencer Burke <info.spencer.burke@gmail.com>
2020-10-15 22:19:46 -0700

    Adding small amounts of code to improve the skeleton of the application

--- dtm.py
+++ dtm.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python
+import os
+import subprocess
+
+# gather config information from conf file
+def conf():
+    pass
+
+# pull the dotfiles from the git repo
+def git_pull():
+    pass
 
 # main function
 def main():
