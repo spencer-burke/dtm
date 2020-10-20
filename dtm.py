4f7db9512049db23053092ff21607ae80a2193f3
Spencer Burke <info.spencer.burke@gmail.com>
2020-10-19 23:22:28 -0700

    Added more skeleton functions as application is still in planning phase. Added the argparse dependency for future command line usage(this will not have a gui). Changed format of program so all subroutines are defined under the main function.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
      modified:   dtm.py

--- dtm.py
+++ dtm.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python
 import os
 import subprocess
+import argparse
+
+if __name__ == "__main__":
+    main()
+
+# check if dtm has been run on this machine
+def is_first_time_start_up():
+    pass
+
+# setup configuration files and generate default config file for first time use
+def first_time_conf():
+    pass
 
 # gather config information from conf file
 def conf():
@@ -9,11 +21,4 @@ def conf():
 # pull the dotfiles from the git repo
 def git_pull():
     pass
-
-# main function
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
 
