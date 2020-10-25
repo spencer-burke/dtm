500835a497ecdd95a643b5bbc091439d0f0269a6
Spencer Burke <info.spencer.burke@gmail.com>
2020-10-25 16:50:52 -0700

    Just adding more skeleton functions.

--- conf.py
+++ conf.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python
-# nothing here yet until the config file is determined
+
+# generates the path to the config file
+def gen_conf_path():
+    pass
+
+# loads the configuration settings from the conf file
+def load_conf_settings(path):
+    '''
+    path(String): the path to the conf file
+    '''
+    pass
+
