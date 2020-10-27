579236488ea8f1a11b273ad45ca92ddc6d139683
Spencer Burke <info.spencer.burke@gmail.com>
2020-10-26 23:59:25 -0700

    Adding some functionality to the git_pull method

--- dtm.py
+++ dtm.py
@@ -15,6 +15,10 @@ def first_time_conf():
     pass
 
 # pull the dotfiles from the git repo
-def git_pull():
-    pass
+def git_pull(repo):
+    '''
+    repo(String): the repo the dot files are going to be pulled down from
+    '''
+    command_string = f"git pull {repo}"
+    subprocess.check_output(command_string, check=True, stdout=PIPE)   
 
